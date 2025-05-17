e1aa49b8e132cd043cbaf037956724e4226b8dea
dhabar <dhabarcllaahi12@gmail.com>
2025-05-17 14:23:38 +0300

    second comit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <div className=" flex flex-col justify-center text-center mb-10 mt-10 p-5 container">
+    <div className=" flex flex-col justify-center text-center mb-10 mt-10 p-5 container ">
       <p>&copy; dhabar cllaahi</p>
       <h1>{new Date().toLocaleDateString('en-US', { weekday: 'long', month: '2-digit', year: 'numeric',  })}</h1>
     </div>
